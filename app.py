5763f21ab6c9440e0564526ecd76d41aa387a36c
Puttipong1234 <Puttipong.lims@gmail.com>
2019-08-27 16:12:02 +0700

    "fix"

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask , render_template , url_for
 
-app = Flask(__name__,static_folder='templates\static')
+app = Flask(__name__,static_folder='templates/static')
 
 @app.route('/')
 def index():
